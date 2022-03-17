8e564fe75bee722e17089da6832845eeff933634
Tomáš Kelbasa <91247802+TomasKelbasa@users.noreply.github.com>
2022-03-17 11:42:43 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -3,28 +3,40 @@ probihazavod = False
 probehnuto = False
 casstartu = 0
 caskonce = 0
-basic.clear_screen()
+init()
+
+def init():
+    probihazavod = False
+    probehnuto = False
+    casstartu = 0
+    caskonce = 0
+    basic.clear_screen()
+    on_button_pressed_a()
 
 def on_forever():
-    if input.logo_is_pressed():
-        radio.send_number(0)
-        music.play_tone(Note.BB, music.beat())
-        control.reset()
+    pass
 basic.forever(on_forever)
+
+def on_logo_event_touched():
+    radio.send_number(0)
+    music.play_tone(Note.BB, music.beat())
+    init()
+input.on_logo_event(TouchButtonEvent.TOUCHED, on_logo_event_touched)
 
 def on_button_pressed_a():
     RunComp.set_light_level()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    basic.show_number((caskonce - casstartu) / 1000)
+    if probehnuto and not probihazavod:
+        basic.show_number((caskonce - casstartu) / 1000)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_received_number(receivedNumber):
     global probihazavod, casstartu, caskonce
     if receivedNumber == 0:
         music.play_tone(Note.BB, music.beat())
-        control.reset()
+        init()
     if receivedNumber == 1:
         probihazavod = True
         casstartu = control.millis()
@@ -34,15 +46,17 @@ def on_received_number(receivedNumber):
 radio.on_received_number(on_received_number)
 
 def on_light_drop():
-    global probehnuto, caskonce, casstartu
+    global probehnuto, caskonce, casstartu, probihazavod
     if not probehnuto:
         if probihazavod:
             music.play_tone(Note.C, music.beat())
             radio.send_number(2)
             caskonce = control.millis()
+            probihazavod = False
         else:
             casstartu = control.millis()
             radio.send_number(1)
             music.play_tone(Note.E, music.beat())
+            probihazavod = True
     probehnuto = True
 RunComp.on_light_drop(on_light_drop)
--- main.ts
+++ main.ts
@@ -3,26 +3,40 @@ let probihazavod = false
 let probehnuto = false
 let casstartu = 0
 let caskonce = 0
-basic.clearScreen()
+init()
+function init() {
+    let probihazavod = false
+    let probehnuto = false
+    let casstartu = 0
+    let caskonce = 0
+    basic.clearScreen()
+    on_button_pressed_a()
+}
+
 basic.forever(function on_forever() {
-    if (input.logoIsPressed()) {
-        radio.sendNumber(0)
-        music.playTone(Note.Bb, music.beat())
-        control.reset()
-    }
     
 })
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    RunComp.SetLightLevel()
+input.onLogoEvent(TouchButtonEvent.Touched, function on_logo_event_touched() {
+    radio.sendNumber(0)
+    music.playTone(Note.Bb, music.beat())
+    init()
 })
+function on_button_pressed_a() {
+    RunComp.SetLightLevel()
+}
+
+input.onButtonPressed(Button.A, on_button_pressed_a)
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    basic.showNumber((caskonce - casstartu) / 1000)
+    if (probehnuto && !probihazavod) {
+        basic.showNumber((caskonce - casstartu) / 1000)
+    }
+    
 })
 radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
     
     if (receivedNumber == 0) {
         music.playTone(Note.Bb, music.beat())
-        control.reset()
+        init()
     }
     
     if (receivedNumber == 1) {
@@ -43,10 +57,12 @@ RunComp.OnLightDrop(function on_light_drop() {
             music.playTone(Note.C, music.beat())
             radio.sendNumber(2)
             caskonce = control.millis()
+            probihazavod = false
         } else {
             casstartu = control.millis()
             radio.sendNumber(1)
             music.playTone(Note.E, music.beat())
+            probihazavod = true
         }
         
     }
