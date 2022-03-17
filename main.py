26b14bd840ed908e713be74ab4272334d9a233a6
Tomáš Kelbasa <91247802+TomasKelbasa@users.noreply.github.com>
2022-03-17 11:09:44 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 radio.set_group(8)
 probihazavod = False
 probehnuto = False
-casstartu, caskonce = 0, 0
+casstartu = 0
+caskonce = 0
 basic.clear_screen()
 
 def on_forever():
-    pass
+    if input.logo_is_pressed():
+        radio.send_number(0)
+        music.play_tone(Note.BB, music.beat())
+        control.reset()
 basic.forever(on_forever)
 
 def on_button_pressed_a():
@@ -13,23 +17,31 @@ def on_button_pressed_a():
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    basic.show_number((caskonce-casstartu)/100)
+    basic.show_number((caskonce - casstartu) / 1000)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_received_number(receivedNumber):
-    global probihazavod, casstartu
+    global probihazavod, casstartu, caskonce
+    if receivedNumber == 0:
+        music.play_tone(Note.BB, music.beat())
+        control.reset()
     if receivedNumber == 1:
         probihazavod = True
         casstartu = control.millis()
+    if receivedNumber == 2:
+        probihazavod = False
+        caskonce = control.millis()
 radio.on_received_number(on_received_number)
 
 def on_light_drop():
-    global probehnuto, casstartu, caskonce
+    global probehnuto, caskonce, casstartu
     if not probehnuto:
         if probihazavod:
             music.play_tone(Note.C, music.beat())
+            radio.send_number(2)
             caskonce = control.millis()
         else:
+            casstartu = control.millis()
             radio.send_number(1)
             music.play_tone(Note.E, music.beat())
     probehnuto = True
--- main.ts
+++ main.ts
@@ -1,22 +1,38 @@
 radio.setGroup(8)
 let probihazavod = false
 let probehnuto = false
-let [casstartu, caskonce] = [0, 0]
+let casstartu = 0
+let caskonce = 0
 basic.clearScreen()
 basic.forever(function on_forever() {
+    if (input.logoIsPressed()) {
+        radio.sendNumber(0)
+        music.playTone(Note.Bb, music.beat())
+        control.reset()
+    }
     
 })
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     RunComp.SetLightLevel()
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    basic.showNumber((caskonce - casstartu) / 100)
+    basic.showNumber((caskonce - casstartu) / 1000)
 })
 radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    
+    if (receivedNumber == 0) {
+        music.playTone(Note.Bb, music.beat())
+        control.reset()
+    }
     
     if (receivedNumber == 1) {
         probihazavod = true
         casstartu = control.millis()
+    }
+    
+    if (receivedNumber == 2) {
+        probihazavod = false
+        caskonce = control.millis()
     }
     
 })
@@ -25,8 +41,10 @@ RunComp.OnLightDrop(function on_light_drop() {
     if (!probehnuto) {
         if (probihazavod) {
             music.playTone(Note.C, music.beat())
+            radio.sendNumber(2)
             caskonce = control.millis()
         } else {
+            casstartu = control.millis()
             radio.sendNumber(1)
             music.playTone(Note.E, music.beat())
         }
